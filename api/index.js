b12601b33893dddc974ad8506bbc5b27a5f57916
Belchior <belchiorbenilson@gmail.com>
2024-04-25 20:52:02 +0100

    conectado a base de dados

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('conectado')
+}).catch((error)=>{
+    console.log(error)
+})
 
 app.listen(3000, ()=>{
     console.log('Servidor no ar')
